c943286a80813fbf3da47c5792280f0dde97128e
Przemo <przemo06133@gmail.com>
2025-04-02 22:30:36 +0200

    Split documents

--- load_data.py
+++ load_data.py
@@ -1,4 +1,7 @@
-from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+
 DATA_PATH = "data"
 
 
@@ -9,7 +12,19 @@ def load_documents():
     return document_loader.load()
 
 
-print("Call function")
-res = load_documents()
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
 
-print(res)
+
+print("Call function")
+loaded_documents = load_documents()
+chunks = split_documents(loaded_documents)
+
+print(chunks[0])
+print(len(chunks))
